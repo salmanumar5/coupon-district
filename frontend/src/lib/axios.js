1165000d765e6fcdad4b4a86cb21a9c47a4ef182
salmanumar5 <umarsalman555@gmail.com>
2025-03-16 21:10:06 +0530

    forntend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "https://coupon-district.onrender.com/api" : "/api",
     withCredentials: true,
 });
 
